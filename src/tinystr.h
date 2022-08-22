25ddb46c5e5c6eb55645f1e103b13d62d1c52540
Enchan <enchant0130@gmail.com>
2022-08-22 15:54:07 +0900

    [Add #3] string list

--- src/tinystr.h
+++ src/tinystr.h
@@ -12,6 +12,15 @@ typedef struct {
     char* value;           // 値へのポインタ
 } string_t;
 
+/**
+ * @struct string_list
+ * @brief 文字列配列
+ */
+struct string_list {
+    unsigned long count;  // 要素数
+    string_t* value;      // 値へのポインタ
+};
+
 // ---
 
 int init_string(string_t*, const char*);
@@ -20,3 +29,14 @@ void free_string(string_t*);
 int set_string(string_t*, const char*);
 
 void dump_string(string_t*);
+
+// ---
+
+int init_string_list(struct string_list*, const unsigned int);
+void free_string_list(struct string_list*);
+
+int add_string_list(struct string_list*, const char*);
+int set_string_list(struct string_list*, const unsigned int, const char*);
+void dump_string_list(struct string_list*);
+
+char** char_repr_string_list(struct string_list*);
