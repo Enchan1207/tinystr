d52096a35306b6e3a072b1c3151879b35cde5f59
Enchan <enchant0130@gmail.com>
2022-08-22 15:07:28 +0900

    [Modify] files

---
+++ src/tinystr.c
@@ -0,0 +1,4 @@
+//
+//
+//
+#include "tinystr.h"
---
+++ tests/test_tinystr.cpp
@@ -0,0 +1,12 @@
+//
+//
+//
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "tinystr.h"
+}
+
+TEST(TinyStrTest, testHandleString) {
+    /* testcase */
+}
